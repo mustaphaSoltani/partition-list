cd170cf0b2825720b38fe4752c178c97de6698b5
user <mustaphasoltani@gmail.com>
2020-06-10 01:46:36 +0200

    first commit

--- src/main/java/com/adneom/partition/PartitionMain.java
+++ src/main/java/com/adneom/partition/PartitionMain.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class PartitionMain {
     public static void main(String[] args) {
         List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5);
-        System.out.println(Partition.partition(inputList,2));
+        System.out.println(Partition.partition(inputList, 2));
     }
 
 
--- src/test/java/com/adneom/partition/PartitionTest.java
+++ src/test/java/com/adneom/partition/PartitionTest.java
@@ -1,26 +1,24 @@
 package com.adneom.partition;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class PartitionTest {
 
     @Test
-    public void testListe(){
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i < 6; i++) {
-            list.add(i);
+    public void testListe() {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            resultList.add(i);
         }
-        ArrayList<ArrayList<Integer>> expected = new ArrayList<> ();
-        expected.add(new ArrayList<>(Arrays.asList(1,2)));
-        expected.add(new ArrayList<>(Arrays.asList(3,4)));
-        expected.add(new ArrayList<>(Collections.singletonList( 5)));
-        assertEquals(expected, Partition.partition(list, 2));
+        List<List<Integer>> expectedList = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3, 4), Collections.singletonList(5));
+        assertEquals(expectedList, Partition.partition(resultList, 2));
 
     }
 
